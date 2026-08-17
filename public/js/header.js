e350e1a74f1c636ff55da4781889df25a5ce3d86
alexis <alexiscrotte19-del>
2026-08-17 17:52:34 +0200

    changement effectuer

---
+++ public/js/header.js
@@ -0,0 +1,26 @@
+// js/header.js
+class SiteHeader extends HTMLElement {
+  connectedCallback() {
+    // Récupère le nom du fichier actuel (ex: "projets.html" ou "" pour l'accueil)
+    const currentPath = window.location.pathname.split("/").pop() || "index.html";
+
+    this.innerHTML = `
+      <header>
+        <a href="index.html">Alexis Crotte</a>
+        <nav>
+          <button id="themeToggle" type="button" aria-label="Changer de thème">🌓</button>
+          <ul>
+            <li><a href="experience.html" class="${currentPath === 'experience.html' ? 'active' : ''}">Expérience</a></li>
+            <li><a href="competences.html" class="${currentPath === 'competences.html' ? 'active' : ''}">Compétences</a></li>
+            <li><a href="projets.html" class="${currentPath === 'projets.html' ? 'active' : ''}">Projets</a></li>
+            <li><a href="formation.html" class="${currentPath === 'formation.html' ? 'active' : ''}">Formation</a></li>
+            <li><a href="actualites.html" class="${currentPath === 'actualites.html' ? 'active' : ''}">Actualitéss</a></li>
+            <li><a href="contact.html" class="${currentPath === 'contact.html' ? 'active' : ''}">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define("site-header", SiteHeader);
